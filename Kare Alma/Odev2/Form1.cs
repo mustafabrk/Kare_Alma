4ab32d2c9b9688c97e2a14bdb1490c97402d8193
mustafabrk <mustafaburaksert@hotmail.com>
2021-04-26 19:13:57 +0300

    Kare Alma
    Nesneye Dayalı Programlama dersinin vizesi için hazırladığım basit bir sayının karesini alan form uygulaması

---
+++ Kare Alma/Odev2/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            UstHesap u1 = new UstHesap();
+            int sonuc = u1.Hesapla(Convert.ToInt32(textBox1.Text));
+            textBox2.Text = sonuc.ToString();
+        }
+    }
+}
---
+++ Kare Alma/Odev2/UstHesap.cs
@@ -0,0 +1,15 @@
+﻿namespace Odev2
+{
+    class UstHesap
+    {
+        private int sayi;
+        private int sonuc;
+
+        public int Hesapla(int _sayi)
+        {
+            sayi = _sayi;
+            sonuc = sayi * sayi;
+            return sonuc;
+        }
+    }
+}
